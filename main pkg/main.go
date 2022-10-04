a12d056cc0aa4955c24a32ef381acc8636d96460
JCPrice0024 <jcprice0024@gmail.com>
2022-10-04 13:13:38 -0400

    Updated info for pal func

---
+++ main pkg/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"../testcode2"
+)
+
+func main() {
+	data := "racecar..."
+	output := testcode2.Pal(data)
+	fmt.Println(output)
+
+}
--- testcode2.go
+++ testcode2.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // func pal takes an array of runes and determines whether or not
 // their value is a palindrome
@@ -8,10 +10,10 @@ func pal(a string) bool {
 	vals := []rune(a)
 	back := len(vals) - 1
 	for front := 0; front <= back; front++ {
-		f := vals[front]
-		b := vals[back]
-		front = findValidFront(vals, front, back)
-		back = findValidBack(vals, back, front)
+		f := ' '
+		b := ' '
+		front, f = findValidFront(vals, front, back)
+		back, b = findValidBack(vals, back, front)
 		if f != b {
 			return false
 		}
@@ -20,56 +22,43 @@ func pal(a string) bool {
 	return true
 }
 
-// frontLowerCaseAscii takes an array of runes and the current front
+// lowerCaseAscii takes an array of runes and the current front
 // and returns the lowercase ascii value of the rune
-func frontLowerCaseAscii(vals []rune, front int, back int) int {
-	for f := vals[front]; f >= 'A' && f <= 'Z'; {
-		f = 'a' + f - 'A'
+func lowerCaseAscii(val rune) rune {
+	if val >= 'A' && val <= 'Z' {
+		val = 'a' + val - 'A'
 	}
-	return back
+	return val
 }
 
-// backLowerCaseAscii takes an array of runes and the current back
-// and returns the lowercase ascii value of the rune
-func backLowerCaseAscii(vals []rune, back int, front int) int {
-
-	for b := vals[back]; b >= 'A' && b <= 'Z'; {
-		b = 'a' + b - 'A'
-	}
-	return front
-}
-
-// findValidBack takes an array of runes, and the current front, and back, and returns
+// findValidFront takes an array of runes, and the current front, and back, and returns
 // index of the next element that contains a valid ascii number or letter
-func findValidFront(vals []rune, front int, back int) int {
+func findValidFront(vals []rune, front int, back int) (int, rune) {
 	for ; front <= back; front++ {
-		f := vals[front]
-		front = frontLowerCaseAscii(vals, front, back)
+		f := lowerCaseAscii(vals[front])
 
 		if (f >= 'a' && f <= 'z') || (f >= '0' && f <= '9') {
-			return front
+			return front, f
 		}
 	}
-	return back
+	return back, vals[back]
 }
 
 // findValidBack takes an array of runes, and the current front, and back, and returns
 // index of the next element that contains a valid ascii number or letter
-func findValidBack(vals []rune, back int, front int) int {
+func findValidBack(vals []rune, back int, front int) (int, rune) {
 	for ; back >= front; back-- {
-		b := vals[back]
-		back = backLowerCaseAscii(vals, back, front)
+		b := lowerCaseAscii(vals[back])
 
 		if (b >= 'a' && b <= 'z') || (b >= '0' && b <= '9') {
-			return back
+			return back, b
 		}
 
 	}
-	return front
+	return front, vals[front]
 }
 func main() {
-	data := "a man, a plan, a canal: Panama"
+	data := "bracecar..."
 	output := pal(data)
 	fmt.Println(output)
-
 }
