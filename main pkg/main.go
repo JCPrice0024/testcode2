650c49f14e5242f1f4782e7df85da49277c6b65c
JCPrice0024 <jcprice0024@gmail.com>
2022-10-07 15:55:28 -0400

    Made infinite for loop so you can check multiple values before the program exits. To exit use end

--- main pkg/main.go
+++ main pkg/main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"testcode2"
 )
 
 func main() {
-	data := testcode2.UserInput()
-	output := testcode2.Palindrome(data)
-	fmt.Println(output)
+	for {
+		data := testcode2.UserInput()
+		strings.TrimSpace(data)
+		output := testcode2.Palindrome(data)
+		if data == "end" {
+			return
+		} else {
+			output = testcode2.Palindrome(data)
+		}
+		fmt.Println(output)
+	}
+
+	//output := testcode2.Palindrome(data)
+	//fmt.Println(output)
 }
